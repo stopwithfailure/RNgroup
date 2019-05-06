9ad12379bec351183256b62d52b5c5a31a414cf2
Qin6468 <Qin6468@github.com>
2019-05-06 10:45:30 +0800

    to get the date

--- App.js
+++ App.js
@@ -13,6 +13,17 @@ import {
 
 const { width, height } = Dimensions.get('window');
 
+//To get the year date and time
+const Getdate=()=>{
+    var date = new Date();
+    var year = date.getFullYear().toString();
+    var month = (date.getMonth()+1).toString();
+    var day = date.getDate().toString();
+    var hour =  date.getHours().toString();
+    var minute = date.getMinutes().toString();
+    return year+'年'+month+'月'+day+'日'+' '+hour+':'+minute;
+}
+
 export default class RNProjectTestApp extends Component {
     constructor(props) {
         super(props);
