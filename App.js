94f57e64ce8ad280ec10b77777c16a0674fede1a
stopwithfailure <stopwithfailure@github.com>
2019-05-27 20:20:08 +0800

    finished

--- App.js
+++ App.js
@@ -1,35 +1,202 @@
 import React, { Component } from 'react';
+
 import {
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-    FlatList,
-    Dimensions,
-    Image,
-    Button,
-    TextInput,
+	FlatList,
+	AppRegistry,
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	PixelRatio,
 } from 'react-native';
+//1. 导入文件
 
-const { width, height } = Dimensions.get('window');
+import { createStackNavigator,createAppContainer } from 'react-navigation';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+// import Ionicons from 'react-native-vector-icons/Ionicons';
+const URL="http://172.22.125.5/api/api.php?";
 
-export default class RNProjectTestApp extends Component {
-    constructor(props) {
+//2.创建一些页面
+class province extends React.Component {
+	static navigationOptions = {
+		title: 'PROVINCE LIST',
+	};
+	 
+	constructor(props) {
         super(props);
-    }
-    
-    render() {
-        return (
-            <View style={{ flex: 0 }}>
-                
-            </View>
-        );
-    }
+        this.state = {
+            data: [],//this is the state of user's data
+        };
+	}
+	
+	componentDidMount(){
+		fetch(URL+"query=province")
+		.then((response) => response.json())        // json
+		.then((responseData) => {   
+			this.setState({
+				data: responseData,//when we use setState,it render again 
+			})
+		})
+		.catch((error) => {     // handle err 
 
-    
+		})
+		.done();
+	}
+	renderItem(item){
+		const { navigate } = this.props.navigation;
+		return(
+			<View>
+				<TouchableOpacity onPress={()=>navigate('city',{value:item.province})}>
+					<Text>{item.province}</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+	render() {
+		return(
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<FlatList
+                    // ref="flatList"
+                    extraData={this.state}
+                    keyExtractor={(item, index) => String(index)}
+                    data={this.state.data} // data
+                    renderItem={({ item }) => this.renderItem(item)} // row
+                    // ItemSeparatorComponent={this.separatorComponent} // separator
+                    horizontal={false} // row or column
+                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+                />
+				<Text>province</Text>
+			</View>
+		);
+	}
+	
+};
+
+class city extends React.Component {
+	static navigationOptions = {
+		title: "CITY LIST",
+	};
+	constructor(props) {
+        super(props);
+        this.state = {
+            data: [],//this is the state of user's data
+        };
+    }
+	componentDidMount(){
+		fetch(URL+"query=city&"+"province="
+		+this.props.navigation.state.params.value)
+		.then((response) => response.json())        // json
+		.then((responseData) => {   
+			this.setState({
+				data: responseData,//when we use setState,it render again 
+			})
+		})
+		.catch((error) => {     // handle err 
+
+		})
+		.done();
+	}
+	renderItem(item){
+		const { navigate } = this.props.navigation;
+		return(
+			<View>
+				<TouchableOpacity onPress={()=>navigate('details',{
+					province_1:this.props.navigation.state.params.value,
+					city_1:item.city})}>
+				<Text>{item.city}</Text>
+				</TouchableOpacity>
+			</View>
+		);
+	}
+	render() {
+		return(
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<FlatList
+                    // ref="flatList"
+                    extraData={this.state}
+                    keyExtractor={(item, index) => String(index)}
+                    data={this.state.data} // data
+                    renderItem={({ item }) => this.renderItem(item)} // row
+                    // ItemSeparatorComponent={this.separatorComponent} // separator
+                    horizontal={false} // row or column
+                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+                />
+				<Text>province</Text>
+			</View>
+		);
+	}
 }
 
-const styles = StyleSheet.create({
-    
+
+
+class details extends React.Component {
+	static navigationOptions = {
+		title: "DETAILS LIST",
+	};
+	constructor(props) {
+        super(props);
+        this.state = {
+            data: [],//this is the state of user's data
+        };
+    }
+	componentDidMount(){
+		fetch(URL+"query=scenic_spot&"+"province="
+		+this.props.navigation.state.params.province_1+"&city="
+		+this.props.navigation.state.params.city_1)
+		.then((response) => response.json())        // json
+		.then((responseData) => {   
+			this.setState({
+				data: responseData,//when we use setState,it render again 
+			})
+		})
+		.catch((error) => {     // handle err 
+
+		})
+		.done();
+	}
+	renderItem(item){
+		return(
+			<View>
+				<Text>{item.spot_name}</Text>
+				<Text>{item.img_uri}</Text>
+				<Text>{item.introduction}</Text>
+			</View>
+		);
+	}
+	render() {
+		return(
+			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<FlatList
+                    // ref="flatList"
+                    extraData={this.state}
+                    keyExtractor={(item, index) => String(index)}
+                    data={this.state.data} // data
+                    renderItem={({ item }) => this.renderItem(item)} // row
+                    // ItemSeparatorComponent={this.separatorComponent} // separator
+                    horizontal={false} // row or column
+                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+                />
+				<Text>province</Text>
+			</View>
+		);
+	}
+}
+//3. 添加TabNavigator中
+const RootTabs = createStackNavigator({
+	province: {
+		screen: province,
+	},
+	city: {
+		screen: city,
+	},
+	details: {
+		screen: details,
+	}
 });
-AppRegistry.registerComponent('RNProjectTestApp', () => RNProjectTestApp);
+
+const App=createAppContainer(RootTabs);
+//最后导出
+export default App;
