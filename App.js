0d189cc73bf10cb713f4c724747484039e992493
stopwithfailure <stopwithfailure@github.com>
2019-05-29 16:29:14 +0800

    search function

--- App.js
+++ App.js
@@ -13,79 +13,92 @@ import {
 } from 'react-native';
 //1. 导入文件
 
-import { createStackNavigator,createAppContainer } from 'react-navigation';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { TouchableOpacity, TextInput } from 'react-native-gesture-handler';
 // import Ionicons from 'react-native-vector-icons/Ionicons';
-const URL="http://10.12.33.161/api/api.php?";
+const URL = "http://10.12.33.161/api/api.php?";
 // const URL="http://245786ka89.zicp.vip/api/api.php?";
 // const URL="http://192.168.1.104/api/api.php?";
-
+var netdata = [];
+var netdata_c = [];
 //2.创建一些页面
 class province extends React.Component {
 	static navigationOptions = {
 		title: 'PROVINCE LIST',
 	};
-	 
+
 	constructor(props) {
-// //To get the year date and time
-// const Getdate=()=>{
-//     var date = new Date();
-//     var year = date.getFullYear().toString();
-//     var month = (date.getMonth()+1).toString();
-//     var day = date.getDate().toString();
-//     var hour =  date.getHours().toString();
-//     var minute = date.getMinutes().toString();
-//     return year+'年'+month+'月'+day+'日'+' '+hour+':'+minute;
-// }
 
-        super(props);
-        this.state = {
-            data: [],//this is the state of user's data
-        };
+		super(props);
+		this.state = {
+			data: [],//this is the state of user's data
+		};
 	}
-	
-	componentDidMount(){
-		fetch(URL+"query=province")
-		.then((response) => response.json())        // json
-		.then((responseData) => {   
-			this.setState({
-				data: responseData,//when we use setState,it render again 
+
+	componentDidMount() {
+		fetch(URL + "query=province")
+			.then((response) => response.json())        // json
+			.then((responseData) => {
+				this.setState({
+					data: responseData,//when we use setState,it render again 
+				})
+				netdata = responseData;
 			})
-		})
-		.catch((error) => {     // handle err 
+			.catch((error) => {     // handle err 
 
-		})
-		.done();
+			})
+			.done();
 	}
-	renderItem(item){
+
+	search_(clue) {
+		// Alert.alert(clue)
+		if(clue == ""){
+			this.setState({
+				data: netdata,
+			})
+		}else{
+			let newdata = [];
+			for(let i = 0; i < netdata.length; ++i){
+				// Alert.alert(netdata[i]['province'])	;
+				if(netdata[i]['province'].toLowerCase().indexOf(clue) != -1) {
+					newdata.push(netdata[i]);
+				}
+			}
+			this.setState({
+				data: newdata,
+			})
+		}
+	}
+	renderItem(item) {
 		const { navigate } = this.props.navigation;
-		return(
+		return (
 			<View>
-				<TouchableOpacity onPress={()=>navigate('city',{value:item.province})}>
-					<Text style={{fontSize:23}}>{item.province}</Text>
+				<TouchableOpacity onPress={() => navigate('city', { value: item.province })}>
+					<Text style={{ fontSize: 23 }}>{item.province}</Text>
 				</TouchableOpacity>
 			</View>
 		);
 	}
 	render() {
-		return(
+		return (
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<TextInput onChangeText={ (clue) => this.search_(clue)}></TextInput>
 				<FlatList
-                    // ref="flatList"
-                    extraData={this.state}
-                    keyExtractor={(item, index) => String(index)}
-                    data={this.state.data} // data
-                    renderItem={({ item }) => this.renderItem(item)} // row
-                    // ItemSeparatorComponent={this.separatorComponent} // separator
-                    horizontal={false} // row or column
-                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
-                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
-                />
+					// ref="flatList"
+					extraData={this.state}
+					keyExtractor={(item, index) => String(index)}
+					data={this.state.data} // data
+					renderItem={({ item }) => this.renderItem(item)} // row
+					// ItemSeparatorComponent={this.separatorComponent} // separator
+					horizontal={false} // row or column
+					// ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+					numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+				/>
 				<Text>provinces</Text>
 			</View>
 		);
 	}
-	
+
 };
 
 class city extends React.Component {
@@ -93,51 +106,73 @@ class city extends React.Component {
 		title: "CITY LIST",
 	};
 	constructor(props) {
-        super(props);
-        this.state = {
-            data: [],//this is the state of user's data
-        };
-    }
-	componentDidMount(){
-		fetch(URL+"query=city&"+"province="
-		+this.props.navigation.state.params.value)
-		.then((response) => response.json())        // json
-		.then((responseData) => {   
-			this.setState({
-				data: responseData,//when we use setState,it render again 
-			})
-		})
-		.catch((error) => {     // handle err 
-
-		})
-		.done();
+		super(props);
+		this.state = {
+			data: [],//this is the state of user's data
+		};
 	}
-	renderItem(item){
+	componentDidMount() {
+		fetch(URL + "query=city&" + "province="
+			+ this.props.navigation.state.params.value)
+			.then((response) => response.json())        // json
+			.then((responseData) => {
+				this.setState({
+					data: responseData,//when we use setState,it render again 
+				})
+				netdata_c = responseData;
+			})
+			.catch((error) => {     // handle err 
+
+			})
+			.done();
+	}
+	search_(clue) {
+		// Alert.alert(clue)
+		if(clue == ""){
+			this.setState({
+				data: netdata_c,
+			})
+		}else{
+			let newdata = [];
+			for(let i = 0; i < netdata_c.length; ++i){
+				// Alert.alert(netdata[i]['province'])	;
+				if(netdata_c[i]['city'].toLowerCase().indexOf(clue) != -1) {
+					newdata.push(netdata_c[i]);
+				}
+			}
+			this.setState({
+				data: newdata,
+			})
+		}
+	}
+	renderItem(item) {
 		const { navigate } = this.props.navigation;
-		return(
+		return (
 			<View>
-				<TouchableOpacity onPress={()=>navigate('details',{
-					province_1:this.props.navigation.state.params.value,
-					city_1:item.city})}>
-				<Text style={{fontSize:23}}>{item.city}</Text>
+				<TouchableOpacity onPress={() => navigate('details', {
+					province_1: this.props.navigation.state.params.value,
+					city_1: item.city
+				})}>
+					<Text style={{ fontSize: 23 }}>{item.city}</Text>
 				</TouchableOpacity>
 			</View>
 		);
 	}
 	render() {
-		return(
+		return (
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+				<TextInput onChangeText={ (clue) => this.search_(clue)}></TextInput>
 				<FlatList
-                    // ref="flatList"
-                    extraData={this.state}
-                    keyExtractor={(item, index) => String(index)}
-                    data={this.state.data} // data
-                    renderItem={({ item }) => this.renderItem(item)} // row
-                    // ItemSeparatorComponent={this.separatorComponent} // separator
-                    horizontal={false} // row or column
-                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
-                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
-                />
+					// ref="flatList"
+					extraData={this.state}
+					keyExtractor={(item, index) => String(index)}
+					data={this.state.data} // data
+					renderItem={({ item }) => this.renderItem(item)} // row
+					// ItemSeparatorComponent={this.separatorComponent} // separator
+					horizontal={false} // row or column
+					// ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+					numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+				/>
 				<Text>cities</Text>
 			</View>
 		);
@@ -151,52 +186,52 @@ class details extends React.Component {
 		title: "DETAILS LIST",
 	};
 	constructor(props) {
-        super(props);
-        this.state = {
-            data: [],//this is the state of user's data
-        };
-    }
-	componentDidMount(){
-		fetch(URL+"query=scenic_spot&"+"province="
-		+this.props.navigation.state.params.province_1+"&city="
-		+this.props.navigation.state.params.city_1)
-		.then((response) => response.json())        // json
-		.then((responseData) => {   
-			 
-			this.setState({
-				data: responseData,//when we use setState,it render again
-				
-			})
-		})
-		.catch((error) => {     // handle err 
-
-		})
-		.done();
+		super(props);
+		this.state = {
+			data: [],//this is the state of user's data
+		};
 	}
-	renderItem(item){
-		return(
+	componentDidMount() {
+		fetch(URL + "query=scenic_spot&" + "province="
+			+ this.props.navigation.state.params.province_1 + "&city="
+			+ this.props.navigation.state.params.city_1)
+			.then((response) => response.json())        // json
+			.then((responseData) => {
+
+				this.setState({
+					data: responseData,//when we use setState,it render again
+
+				})
+			})
+			.catch((error) => {     // handle err 
+
+			})
+			.done();
+	}
+	renderItem(item) {
+		return (
 			<View>
-				<Text style={{fontSize:20}}>{item.spot_name}</Text>
-				<Image style={{width:400,height:150,}} source={{ uri: item.img_uri }} />
+				<Text style={{ fontSize: 20 }}>{item.spot_name}</Text>
+				<Image style={{ width: 400, height: 150, }} source={{ uri: item.img_uri }} />
 				{/* <Text>{item.img_uri}</Text> */}
 				<Text>{item.introduction}</Text>
 			</View>
 		);
 	}
 	render() {
-		return(
+		return (
 			<View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
 				<FlatList
-                    // ref="flatList"
-                    extraData={this.state}
-                    keyExtractor={(item, index) => String(index)}
-                    data={this.state.data} // data
-                    renderItem={({ item }) => this.renderItem(item)} // row
-                    // ItemSeparatorComponent={this.separatorComponent} // separator
-                    horizontal={false} // row or column
-                    // ListEmptyComponent={this.listEmptyComponent} // show when there is no data
-                    numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
-                />
+					// ref="flatList"
+					extraData={this.state}
+					keyExtractor={(item, index) => String(index)}
+					data={this.state.data} // data
+					renderItem={({ item }) => this.renderItem(item)} // row
+					// ItemSeparatorComponent={this.separatorComponent} // separator
+					horizontal={false} // row or column
+					// ListEmptyComponent={this.listEmptyComponent} // show when there is no data
+					numColumns={1} // set how many columns each row,  you shouldn't use columnWrapperStyle when it is 1
+				/>
 				<Text>scenic spots</Text>
 			</View>
 		);
@@ -215,6 +250,6 @@ const RootTabs = createStackNavigator({
 	}
 });
 
-const App=createAppContainer(RootTabs);
+const App = createAppContainer(RootTabs);
 //最后导出
 export default App;
